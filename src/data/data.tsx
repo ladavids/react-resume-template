1f1e50263e8023b155f8a582b6e161dc5c357fce
ladavids <ldavids2@unb.ca>
2023-08-16 20:54:59 -0300

    Update data.tsx

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -44,8 +44,8 @@ import {
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'React Resume Template',
-  description: "Example site built with Tim Baker's react resume template",
+  title: 'React CV/Resume',
+  description: "A CV project built from a template provided by tbakerx",
 };
 
 /**
@@ -69,18 +69,17 @@ export type SectionId = (typeof SectionId)[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `I'm Tim Baker.`,
+  name: `My name is Logan Davidson.`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a Victoria based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
-        at <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
-        registrar and site builder.
+        I'm a Fredericton based <strong className="text-stone-100">Software Developer</strong>, currently working
+        at <strong className="text-stone-100">Applied Digital (APLD) </strong>.
       </p>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        In my free time time, you can catch me training in <strong className="text-stone-100">Muay Thai</strong>,
-        plucking my <strong className="text-stone-100">banjo</strong>, or exploring beautiful{' '}
-        <strong className="text-stone-100">Vancouver Island</strong>.
+        In my free time time, you can catch me <strong className="text-stone-100">photographing birds and wildlife</strong>
+        , or exploring {' '}
+        <strong className="text-stone-100">Fredericton</strong>.
       </p>
     </>
   ),
@@ -108,12 +107,12 @@ export const aboutData: About = {
   to use or feel most comfortable with, describing your personality, or whatever else you feel like throwing
   in.`,
   aboutItems: [
-    {label: 'Location', text: 'Victoria, BC', Icon: MapIcon},
-    {label: 'Age', text: '29', Icon: CalendarIcon},
-    {label: 'Nationality', text: 'Canadian / Irish', Icon: FlagIcon},
-    {label: 'Interests', text: 'Motorcycles, Muay Thai, Banjos', Icon: SparklesIcon},
-    {label: 'Study', text: 'University of Victoria', Icon: AcademicCapIcon},
-    {label: 'Employment', text: 'Instant Domains, inc.', Icon: BuildingOffice2Icon},
+    {label: 'Location', text: 'Fredericton, NB', Icon: MapIcon},
+    {label: 'Age', text: '30', Icon: CalendarIcon},
+    {label: 'Nationality', text: 'Canadian', Icon: FlagIcon},
+    {label: 'Interests', text: 'Photography, Birds, Urban Wildlife', Icon: SparklesIcon},
+    {label: 'Study', text: 'University of New Brunswick', Icon: AcademicCapIcon},
+    {label: 'Employment', text: 'Applied Digital, inc.', Icon: BuildingOffice2Icon},
   ],
 };
 
@@ -127,15 +126,7 @@ export const skills: SkillGroup[] = [
       {
         name: 'English',
         level: 10,
-      },
-      {
-        name: 'French',
-        level: 4,
-      },
-      {
-        name: 'Spanish',
-        level: 3,
-      },
+      }
     ],
   },
   {
@@ -143,16 +134,20 @@ export const skills: SkillGroup[] = [
     skills: [
       {
         name: 'React',
-        level: 9,
+        level: 3,
       },
       {
         name: 'Typescript',
-        level: 7,
+        level: 2,
       },
       {
-        name: 'GraphQL',
-        level: 6,
+        name: 'Vue.js',
+        level: 3,
       },
+      {
+        name: 'CSS',
+        level: 4,
+      }
     ],
   },
   {
@@ -160,35 +155,61 @@ export const skills: SkillGroup[] = [
     skills: [
       {
         name: 'Node.js',
-        level: 8,
-      },
-      {
-        name: 'Rust',
-        level: 5,
-      },
-      {
-        name: 'Golang',
         level: 4,
+      },
+      {
+        name: 'Python',
+        level: 7,
       },
     ],
   },
   {
-    name: 'Mobile development',
+    name: 'Development',
     skills: [
       {
-        name: 'React Native',
-        level: 9,
+        name: 'Java',
+        level: 5,
       },
       {
-        name: 'Flutter',
-        level: 4,
+        name: 'C#',
+        level: 5,
       },
       {
-        name: 'Swift',
-        level: 3,
-      },
+        name: 'C',
+        level: 5
+      }
     ],
   },
+  {
+    name: 'Operating Systems',
+    skills: [
+      {
+        name: 'Windows',
+        level: 8,
+      },
+      {
+        name: 'Linux',
+        level: 7,
+      },
+      {
+        name: 'MacOS',
+        level: 7,
+      },
+      {
+        name: 'Android',
+        level: 7,
+      },
+      {
+        name: 'iOS',
+        level: 7,
+      },
+      {
+        name: 'iPadOS',
+        level: 8,
+      }
+    ]
+  },
+  
 ];
 
 /**
@@ -196,70 +217,16 @@ export const skills: SkillGroup[] = [
  */
 export const portfolioItems: PortfolioItem[] = [
   {
-    title: 'Project title 1',
-    description: 'Give a short description of your project here.',
+    title: 'ND Spoons',
+    description: 'A game made in a university course with a team of 3 members.',
     url: 'https://reactresume.com',
     image: porfolioImage1,
   },
   {
-    title: 'Project title 2',
-    description: 'Give a short description of your project here.',
+    title: 'Cat SPA',
+    description: 'A Single Page Application made in a university course with a team of 3 members.',
     url: 'https://reactresume.com',
     image: porfolioImage2,
-  },
-  {
-    title: 'Project title 3',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage3,
-  },
-  {
-    title: 'Project title 4',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage4,
-  },
-  {
-    title: 'Project title 5',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage5,
-  },
-  {
-    title: 'Project title 6',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage6,
-  },
-  {
-    title: 'Project title 7',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage7,
-  },
-  {
-    title: 'Project title 8',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage8,
-  },
-  {
-    title: 'Project title 9',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage9,
-  },
-  {
-    title: 'Project title 10',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage10,
-  },
-  {
-    title: 'Project title 11',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage11,
   },
 ];
 
@@ -268,24 +235,36 @@ export const portfolioItems: PortfolioItem[] = [
  */
 export const education: TimelineItem[] = [
   {
-    date: 'April 2007',
-    location: 'Clown college',
-    title: 'Masters in Beer tasting',
+    date: 'January 2019 - May 2023',
+    location: 'University of New Brunswick',
+    title: 'Bachelor of Computer Science',
     content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
   },
   {
-    date: 'March 2003',
-    location: 'School of Business',
-    title: 'What did you study 101',
+    date: '2013 - 2017',
+    location: 'The University of Lethbridge',
+    title: 'Bachelor of Science: Neuroscience',
+    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+  },
+  {
+    date: '2012 - 2013',
+    location: 'University of Alberta',
+    title: 'Open Studies',
+    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+  },
+  {
+    date: '2011 - 2012',
+    location: 'University of Alberta',
+    title: 'Bachelor of Engineering',
     content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
   },
 ];
 
 export const experience: TimelineItem[] = [
   {
-    date: 'March 2010 - Present',
-    location: 'Awesome Development Company',
-    title: 'Senior UX Engineer',
+    date: 'March 2023 - Present',
+    location: 'Applied Digital (NASDAQ: APLD)',
+    title: 'Software Developer - Intern',
     content: (
       <p>
         Describe work, special projects, notable achievements, what technologies you have been working with, and
@@ -294,9 +273,9 @@ export const experience: TimelineItem[] = [
     ),
   },
   {
-    date: 'March 2007 - February 2010',
-    location: 'Garage Startup Studio',
-    title: 'Junior bug fixer',
+    date: 'May 2022 - August 2022',
+    location: 'Centre for Nuclear Research (UNB)',
+    title: 'Co-op Student',
     content: (
       <p>
         Describe work, special projects, notable achievements, what technologies you have been working with, and
@@ -304,31 +283,126 @@ export const experience: TimelineItem[] = [
       </p>
     ),
   },
+  {
+    date: 'Feb 2022 - April 2022',
+    location: 'University of New Brunswick',
+    title: 'Student Consultant',
+    content: (
+      <p>
+        Describe work, special projects, notable achievements, what technologies you have been working with, and
+        anything else that would be useful for an employer to know.
+      </p>
+    ),
+  },
+  {
+    date: 'September 2020 - April 2021',
+    location: 'University of New Brunswick',
+    title: 'Co-op Student',
+    content: (
+      <p>
+        Describe work, special projects, notable achievements, what technologies you have been working with, and
+        anything else that would be useful for an employer to know.
+      </p>
+    ),
+  },
+  {
+    date: 'September 2019 - December 2019',
+    location: 'Canadian Institute of Cybersecurity',
+    title: 'Network Flow Analyst',
+    content: (
+      <p>
+        Describe work, special projects, notable achievements, what technologies you have been working with, and
+        anything else that would be useful for an employer to know.
+      </p>
+    ),
+  },
+  {
+    date: 'April 2018 - November 2018',
+    location: "Dairy Queen",
+    title: "Crew Leader",
+    content: (
+      <p>
+
+      </p>
+    ),
+  },
+  {
+    date: 'July 2017 - November 2017',
+    location: "A1 Lumpers",
+    title: "Lumper",
+    content: (
+      <p>
+
+      </p>
+    )
+  },
+  {
+    date: 'May 2015 - August 2015',
+    location: 'Alberta Provincial Parks',
+    title: "Maintenance Service Work I",
+    content: (
+      <p>
+
+      </p>
+    )
+  },
+  {
+    date: 'May 2014 - August 2014',
+    location: 'Alberta Provincial Parks',
+    title: "Irrigation Technician",
+    content: (
+      <p>
+
+      </p>
+    )
+  },
+  {
+    date: 'May 2013 - August 2013',
+    location: 'Eastern Irrigation District',
+    title: "Maintenance Service Work I",
+    content: (
+      <p>
+
+      </p>
+    )
+  },
+  {
+    date: 'May 2012 - August 2012',
+    location: 'Alberta Provincial Parks',
+    title: "Maintenance Service Work I",
+    content: (
+      <p>
+
+      </p>
+    )
+  },
+  
 ];
 
 /**
  * Testimonial section
  */
-export const testimonial: TestimonialSection = {
-  imageSrc: testimonialImage,
-  testimonials: [
-    {
-      name: 'John Doe',
-      text: 'Use this as an opportunity to promote what it is like to work with you. High value testimonials include ones from current or past co-workers, managers, or from happy clients.',
-      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/169.jpg',
-    },
-    {
-      name: 'Jane Doe',
-      text: 'Here you should write some nice things that someone has said about you. Encourage them to be specific and include important details (notes about a project you were on together, impressive quality produced, etc).',
-      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/14.jpg',
-    },
-    {
-      name: 'Someone else',
-      text: 'Add several of these, and keep them as fresh as possible, but be sure to focus on quality testimonials with strong highlights of your skills/work ethic.',
-      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/69.jpg',
-    },
-  ],
-};
+// ADD These in as I get them or can remember them.
+// export const testimonial: TestimonialSection = {
+//   imageSrc: testimonialImage,
+//   testimonials: [
+//     {
+//       name: 'John Doe',
+//       text: 'Use this as an opportunity to promote what it is like to work with you. High value testimonials include ones from current or past co-workers, managers, or from happy clients.',
+//       image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/169.jpg',
+//     },
+//     {
+//       name: 'Jane Doe',
+//       text: 'Here you should write some nice things that someone has said about you. Encourage them to be specific and include important details (notes about a project you were on together, impressive quality produced, etc).',
+//       image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/14.jpg',
+//     },
+//     {
+//       name: 'Someone else',
+//       text: 'Add several of these, and keep them as fresh as possible, but be sure to focus on quality testimonials with strong highlights of your skills/work ethic.',
+//       image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/69.jpg',
+//     },
+//   ],
+// };
 
 /**
  * Contact section
@@ -336,27 +410,27 @@ export const testimonial: TestimonialSection = {
 
 export const contact: ContactSection = {
   headerText: 'Get in touch.',
-  description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
+  description: 'You can reach me via the following contct information.',
   items: [
     {
       type: ContactType.Email,
-      text: 'reachout@timbaker.me',
-      href: 'mailto:reachout@timbaker.me',
+      text: 'logalexdavid@hotmail.com',
+      href: 'mailto:logalexdavid@hotmail.com',
     },
     {
       type: ContactType.Location,
-      text: 'Victoria BC, Canada',
-      href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
+      text: 'Fredericton NB, Canada',
+      href: 'https://goo.gl/maps/Rj9TcVbiiyvpMfGZ8',
     },
     {
       type: ContactType.Instagram,
-      text: '@tbakerx',
-      href: 'https://www.instagram.com/tbakerx/',
+      text: '@logalexdav',
+      href: 'https://www.instagram.com/logalexdav/',
     },
     {
       type: ContactType.Github,
-      text: 'tbakerx',
-      href: 'https://github.com/tbakerx',
+      text: 'ladavids',
+      href: 'https://github.com/ladavids',
     },
   ],
 };
@@ -365,9 +439,7 @@ export const contact: ContactSection = {
  * Social items
  */
 export const socialLinks: Social[] = [
-  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/tbakerx'},
-  {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'},
-  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/timbakerx/'},
-  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/reactresume/'},
-  {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/TimBakerx'},
+  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/ladavids'},
+  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/logan-davidson-22a543123'},
+  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/logalexdav/'},
 ];
